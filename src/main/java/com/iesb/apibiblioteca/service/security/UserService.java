f07e726aec0a9bbd05eae746396d9185680385ea
RonanR19 <80012742+RonanR19@users.noreply.github.com>
2022-10-06 16:53:25 -0300

    teste

--- src/main/java/com/iesb/apibiblioteca/service/security/UserService.java
+++ src/main/java/com/iesb/apibiblioteca/service/security/UserService.java
@@ -3,9 +3,13 @@ package com.iesb.apibiblioteca.service.security;
 import com.iesb.apibiblioteca.dto.user.UserDTO;
 import com.iesb.apibiblioteca.dto.user.UserDTOResponse;
 import com.iesb.apibiblioteca.exception.AlreadyExistsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.iesb.apibiblioteca.exception.ResourceNotFoundException;
+import com.iesb.apibiblioteca.payload.request.user.ResetPasswordRequest;
+import com.iesb.apibiblioteca.payload.response.generic.GenericResponse;
 
 
 public interface UserService {
     UserDTOResponse save(UserDTO userDTO) throws AlreadyExistsException;
+    GenericResponse updateUserPassword(ResetPasswordRequest request) throws ResourceNotFoundException;
 }
+
